523cc8156e655a8cf524b15185df0f6649ec7d20
Luke Chen <luke.chen.2010@gmail.com>
2020-07-18 23:10:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event) 
+{
+  let cards = []
+  for(let i = 0; i < names.length; i++) 
+  {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return cards
+}
+
+function countDown(start) 
+{
+  while(start > 0) 
+  {
+    console.log(start);
+    start -= 1;
+  }
+  console.log(start);
+}
