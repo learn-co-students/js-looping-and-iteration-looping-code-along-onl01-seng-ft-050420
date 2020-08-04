66d840d9e66f5879735b89093aa6b9a4b51624ed
claudiahalip <claudia4ro@yahoo.com>
2020-08-04 13:04:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+            let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+            messages.push(message);
+        }
+        return messages;
+}
+
+function countDown() {
+    let countup = 0;
+    while(countup < 11) {
+        console.log(countup++);
+    }
+}
