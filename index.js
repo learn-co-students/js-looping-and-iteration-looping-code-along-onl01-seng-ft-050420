b90f9586d693a7b935a68c1b27a8cef877e78585
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-04 14:58:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let writtenCards = [];
+    for (let i = 0; i < names.length; i++) {
+        writtenCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      }
+
+    return writtenCards;
+
+}
+
+function countDown(number) {
+    let i = 0;
+    while (i <= number) {
+        console.log(number--);
+    }
+}
+
