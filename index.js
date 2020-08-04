70bc56426a12b69e771b6fbe2455cbeb367c957c
aaron-schuyler <asky28@gmail.com>
2020-08-04 15:13:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function writeCards(names) {
+  return names.map((val) => `Thank you, ${val}, for the wonderful surprise gift!`)
+}
+
+function countDown(n) {
+  for (let i = n; i >= 0; i--){
+    console.log(i)
+  }
+}
