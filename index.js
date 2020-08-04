b02816173de1f1c2d1e296b78cf56e60c652a350
scottvonscott <skeplin17@gmail.com>
2020-08-04 14:39:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let msgs = []
+ for (let i = 0; i < names.length  ; i++) {
+    msgs.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    };
+ return msgs
+}
+
+function countDown(positiveNum) {
+    let i = 0;
+    while (positiveNum >= i) {
+        console.log(positiveNum)
+        positiveNum--
+
+    }
+}
