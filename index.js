581cd073c14fb8632329e9a905e4896260e23a1c
michael thomas <michael.thomas2014@gmail.com>
2020-08-05 23:12:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(num){
+  while (num >= 0){
+    console.log(num)
+    num--
+  }
+}
+
+function writeCards(gift, event){
+  let cards = []
+  for (let i = 0; i < gift.length; i++) {
+   cards.push(`Thank you, ${gift[i]}, for the wonderful ${event} gift!`);
+  }
+  return cards
+  
+}
