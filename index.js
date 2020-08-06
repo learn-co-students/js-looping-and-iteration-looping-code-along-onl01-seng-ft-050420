b1c6ffb97cf0fa0ceb421d3c48025262b47a4697
Anette Rivera <anettem.rivera@gmail.com>
2020-08-06 03:44:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+// const names = [`Lisa`, `Kaitlin`, `Jan` ]
+// const events = 'surprise'
+
+
+function writeCards(names, events){
+  let messages = [];
+  for (let i = 0; i< names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+  }
+  return messages;
+}
+  
+  writeCards(names, events)
+  
+  
+function countDown(number){
+    let countDownNumber = number;
+    while (countDownNumber >= 0) {
+        console.log(countDownNumber);
+        countDownNumber = countDownNumber-1;
+    }
+}
+
+countDown(number)
