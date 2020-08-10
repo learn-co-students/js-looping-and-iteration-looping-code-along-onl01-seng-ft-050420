f800d9049da686365f209d60959a2696d674aa28
emerykurt <emerylumsden@gmail.com>
2020-08-10 10:35:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let cards = []
+    for (let c = 0; c < names.length; c++) {
+        cards.push(`Thank you, ${names[c]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+function countDown ( number ) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
