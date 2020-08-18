4e34b72b7b944d4b8907f310bb70a337df6fddd1
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-08-18 19:23:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+
+function writeCards( names, birthday) {
+  let cards = []
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`)
+  }
+ return cards
+}
+
+
+function countDown(number) {
+    while (number > 0){
+      console.log(number);
+      number -= 1
+    }
+    console.log(number);
+}
